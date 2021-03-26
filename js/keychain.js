90d3f599450ac927e218630e3787d0af0f08911e
songWinterhold <benoit.sommervogel@tsc.digital>
2021-03-26 15:02:13 +0100

    first commit

---
+++ js/keychain.js
@@ -0,0 +1,36 @@
+// Keychain character
+
+var diceroll = function () {
+  return Math.floor(Math.random() * (6 - 1 + 1)) + 1 + Math.floor(Math.random() * (6 - 1 + 1)) + 1 ;
+}
+
+class Trait {
+  constructor (name) {
+    this.name = "demon chasseur"
+    this.main_attribute = 'p'
+    this.secondary_attribute = 'f'
+    this.upgraded = false
+  }
+}
+
+class Character {
+  constructor (traits) {
+    this.attributes = {'p': -1, 'f': -1, 'm': -1};
+    this.traits = "demon chasseur"
+    this.life = 3
+  }
+  
+  set damage(power) {
+    var damageValue = Math.floor((diceroll + power - this.attributes['p']) / 2);
+    if (damageValue > 0) {
+      this.life =- damageValue;
+    }
+  }
+
+  get skillCheck(action_type, threshold, traitName) {
+    return (Math.floor((diceroll + this.attributes[action_type]) / 2) >= threshold);
+  }
+
+}
+
+
