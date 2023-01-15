8a3ebe664bf4b4b413431f0c746da902bed17d09
Benoit SOMMERVOGEL <benoit.sommervogel@jobteaser.com>
2023-01-15 01:58:21 +0100

    simplified the onready process for image building

--- js/cocktail.js
+++ js/cocktail.js
@@ -6,77 +6,78 @@ var MS_PER_UPDATE = 25;
 var lag = 0;
 
 class ImageBuilder {
-  constructor(width, height, base, spriteList) {
+  constructor(width, height, base, sprite_list) {
     this.canvas = document.createElement("canvas");
     this.ctx = this.canvas.getContext("2d");
     this.canvas.width = width;
     this.canvas.height = height;
-    this.elementTotal = spriteList.length;
-    this.elementCount = 0;
+    this.element_total = sprite_list.length;
+    this.element_count = 0;
     this.base = new Image();
     this.base.onload = function () {
       this.ctx.drawImage(this.base, 0, 0, this.canvas.width, this.canvas.height);
-      this.constructBackground(spriteList)
+      this.constructBackground(sprite_list)
     }.bind(this);
     this.base.src = base;
   }
 
   // expect a list of images with coordinates to add to the background ["spritepath", x, y]
-  constructBackground(spriteList) {
-    var addedElement = [];
-    for (var key in spriteList) {
-      var addedElement = new Image();
-      addedElement.key = key;
-      addedElement.constructor = this;
-      addedElement.onload = function () {
-        this.constructor.ctx.drawImage(this, spriteList[this.key]["x"], spriteList[this.key]["y"], this.width, this.height);
-        this.constructor.elementCount++;
+  constructBackground(sprite_list) {
+    var added_element = [];
+    for (var key in sprite_list) {
+      var added_element = new Image();
+      added_element.key = key;
+      added_element.constructor = this;
+      added_element.onload = function () {
+        this.constructor.ctx.drawImage(this, sprite_list[this.key]["x"], sprite_list[this.key]["y"], this.width, this.height);
+        this.constructor.element_count++;
       }
-      addedElement.src = spriteList[key]["spritePath"];
+      added_element.src = sprite_list[key]["spritePath"];
     }
   }
 
-  ready() {
-    if (this.elementCount >= this.elementTotal) {
-      return true;
-    }
-    return false;
-  }
-
-  getImage() {
-    return this.canvas;
+  onReady(execute, scene) {
+    console.log("here")
+    var loader = new GameObject(0, 0, {"norepeat": false}, scene);
+    var load_image = function(object, delta) {
+      if (this.element_count >= this.element_total) {
+        execute(this.canvas);
+        object.destroy();
+      }
+    }.bind(this)
+    new Trigger(load_image, loader)
   }
 }
 
 class CocktailCanvas {
-  constructor(width, height, bgImage, ratio) {
+  constructor(width, height, bg_image, ratio) {
     this.canvas = document.createElement("canvas");
     this.ctx = this.canvas.getContext("2d");
     this.canvas.width = width * ratio;
     this.canvas.height = height * ratio;
     this.ratio = ratio > 0 ? ratio : 1;
     document.body.appendChild(this.canvas);
-    this.bgReady = false;
-    this.bgImage = new Image();
-    this.bgImage.onload = function () {
-      this.bgReady = true;
+    this.bg_ready = false;
+    this.bg_image = new Image();
+    this.bg_image.onload = function () {
+      this.bg_ready = true;
     }.bind(this);
-    this.bgImage.src = bgImage;
+    this.bg_image.src = bg_image;
     cocanvas = this;
   }
 
-  setCanvasBackground(canvasImage) {
-    this.canvas.width = canvasImage.width * this.ratio;
-    this.canvas.height = canvasImage.height * this.ratio;
-    this.bgImage = canvasImage
+  setCanvasBackground(canvas_image) {
+    this.canvas.width = canvas_image.width * this.ratio;
+    this.canvas.height = canvas_image.height * this.ratio;
+    this.bg_image = canvas_image
   }
 
   render(params) {
-    if (this.bgReady) {
-      this.ctx.drawImage(this.bgImage, 0, 0, this.canvas.width, this.canvas.height);
+    if (this.bg_ready) {
+      this.ctx.drawImage(this.bg_image, 0, 0, this.canvas.width, this.canvas.height);
     }
-    for (var key in currentScene.spriteList) {
-      currentScene.spriteList[key].render(this.ctx, this.ratio);
+    for (var key in currentScene.sprite_list) {
+      currentScene.sprite_list[key].render(this.ctx, this.ratio);
     }
   }
 }
@@ -85,7 +86,7 @@ class CocktailCanvas {
 class Scene {
   constructor() {
     currentScene = this;
-    this.spriteList = {};
+    this.sprite_list = {};
     this.triggerList = {};
   }
 
@@ -94,7 +95,7 @@ class Scene {
   }
 
   addSprite(sprite) {
-    this.spriteList[sprite.id] = sprite;
+    this.sprite_list[sprite.id] = sprite;
   }
 
   addTrigger(trigger) {
@@ -102,7 +103,7 @@ class Scene {
   }
 
   deleteSprite(spriteId) {
-    delete this.spriteList[spriteId]
+    delete this.sprite_list[spriteId]
   }
 
   deleteTrigger(triggerId) {
--- js/woods.js
+++ js/woods.js
@@ -6,40 +6,6 @@ var general_controller_game = new GameObject(0, 0, {"norepeat": false}, game_sce
 var general_controller_pause = new GameObject(0, 0, {"norepeat": false}, pause_scene);
 var canvas = new CocktailCanvas(96 * ratio, 96 * ratio, "assets/images/floor.png", ratio);
 
-var map = [
-  [1, 1, 1, 1, 1, 1],
-  [1, 0, 0, 0, 0, 1],
-  [1, 0, 1, 1, 0, 1],
-  [1, 0, 1, 1, 0, 1],
-  [1, 0, 0, 0, 0, 1],
-  [1, 0, 1, 1, 1, 1],
-  [1, 0, 0, 0, 0, 1],
-  [1, 1, 1, 0, 0, 1],
-  [1, 0, 0, 0, 0, 1],
-  [1, 1, 1, 0, 1, 1],
-  [1, 0, 0, 0, 0, 1],
-  [1, 1, 1, 1, 1, 1],
-];
-
-// var map = [
-//   [0, 0, 0, 0, 0, 0, 0, 0],
-//   [0, 0, 0, 0, 0, 0, 0, 0],
-//   [0, 0, 0, 0, 0, 0, 0, 0],
-//   [0, 0, 0, 0, 0, 0, 0, 0],
-// ];
-
-var mapImage = [];
-for (let i = 0; i < map.length; i++) {
-  for (let j = 0; j < map[i].length; j++) {
-    if (map[i][j] == 1) {
-      mapImage.push({"spritePath": "assets/images/wall.png", "x": j * 16, "y": i * 16});
-    }
-  }
-}
-
-var builder = new ImageBuilder(map[0].length * 16, map.length * 16, "assets/images/floor.png", mapImage);
-var loader = new GameObject(0, 0, {"norepeat": false}, game_scene);
-
 var hero = new GameObject(60 * ratio, 20 * ratio,
   {
   "up": "ArrowUp",
@@ -76,17 +42,40 @@ var pause = function(object, delta) {
   }
 }
 
-var loadImage = function(object, delta) {
-  if (builder.ready()) {
-    canvas.setCanvasBackground(builder.getImage());
-    object.destroy();
+//Create a background matching the collision map to replace the standard one
+var map = [
+  [1, 1, 1, 1, 1, 1],
+  [1, 0, 0, 0, 0, 1],
+  [1, 0, 1, 1, 0, 1],
+  [1, 0, 1, 1, 0, 1],
+  [1, 0, 0, 0, 0, 1],
+  [1, 0, 1, 1, 1, 1],
+  [1, 0, 0, 0, 0, 1],
+  [1, 1, 1, 0, 0, 1],
+  [1, 0, 0, 0, 0, 1],
+  [1, 1, 1, 0, 1, 1],
+  [1, 0, 0, 0, 0, 1],
+  [1, 1, 1, 1, 1, 1],
+];
+
+var mapImage = [];
+for (let i = 0; i < map.length; i++) {
+  for (let j = 0; j < map[i].length; j++) {
+    if (map[i][j] == 1) {
+      mapImage.push({"spritePath": "assets/images/wall.png", "x": j * 16, "y": i * 16});
+    }
   }
 }
+
+var builder = new ImageBuilder(map[0].length * 16, map.length * 16, "assets/images/floor.png", mapImage);
+builder.onReady(function(image) {
+  canvas.setCanvasBackground(image);
+}, game_scene)
+
 
 var moveHero = new Trigger(moveCharacter, hero);
 var swapScene = new Trigger(pause, general_controller_game)
 var swapScene2 = new Trigger(resume, general_controller_pause)
-var load = new Trigger(loadImage, loader)
 game_scene.activate();
 
 // Let's play this game!
